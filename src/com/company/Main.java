ef469db2dcffbfb17ec6fb052eb9d95d369ee426
shade2903 <nightshade2903@gmail.com>
2021-03-26 20:09:56 +0300

    commit three;

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
+// число int n [0, 1000000]
+//2^n = ? (вывести на экран!!!) - String
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sum(1,2,3,4,5,6,7,8,9,0,4564,9679,456,2));
+//        System.out.println(sum(1,2,3,4,5,6,7,8,9,0,4564,9679,456,2));
+//        System.out.println(fact(12));
+//        System.out.println(fact2(5));
+        System.out.println(sumDigts(745));
 
 
     }
@@ -27,7 +32,46 @@ public class Main {
             sum += x;
         }
         return sum;
+    }
 
+    //Рекурсия
+
+    // расчет факториала 5! = 5*4*3*2*1 = 120
+    public static int fact(int n){ // нахождение факториала через рекурсию
+        int fact =1;
+        for(; n > 1; n--){
+            fact*=n;
+
+        }
+        return fact;
+    }
+    // расчет факториала через рекурсию (вызов метода в самом себе)
+    //5! = 5*4!
+    //4!= 3*3!
+    //....
+    //n! = n*(n-1)! = n*(n-1)*(n-2)!
+    public static int fact2(int n){
+        if (n==1){
+            return 1;
+        }
+        return n*fact2(n-1);
+
+    }
+    // n =4
+    // 4*3*2*1
+
+    //Найти сумму цифр числа 1234%10 = 4
+    //1234/10 = 123
+    //123%10 =3
+    //123/10 =12
+    //...
+    // 1%10 =1
+    //1/10 =0
+    public static int sumDigts(int n){
+        if (n ==0) {
+            return 0;
+        }
+        return (n%10) +sumDigts(n/10);
 
     }
 }
