dee2fca9931aec09aecf5d5c5450e5c5208743fe
shade2903 <nightshade2903@gmail.com>
2021-03-26 18:59:29 +0300

    commit second;

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,32 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int a =1;
+        System.out.println(sum(1,2,3,4,5,6,7,8,9,0,4564,9679,456,2));
+
+
+    }
+
+    // ПЕРЕГРУЗКА МЕТОДА sum()
+    //для создания новой перегрузки необходимо либо изменить количество параметров
+    //либо один из типов любого параметра
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static float sum(float a, float b) {
+        return a + b;
+    }
+
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+    public static int sum( int...arr) {
+        int sum = 0;
+        for (int x : arr) {
+            sum += x;
+        }
+        return sum;
+
 
     }
 }
